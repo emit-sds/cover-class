ea3883989691d7719a167d586c3068395754a4d5
makiper <michael.kiper@jpl.nasa.gov>
2025-08-28 16:38:34 -0700

    added more unit testing

--- tests/subsample/subsampler_test.py
+++ tests/subsample/subsampler_test.py
@@ -2,6 +2,7 @@ import unittest
 import numpy as np
 from torch import FloatTensor
 import torch
+from sklearn.decomposition import PCA
 
 from cover_class.subsample import subsampler # type: ignore[import]
 
@@ -15,7 +16,6 @@ class subsampleTest(unittest.TestCase):
         self.num_pc = 3
 
     def test_convex_hull(self):
-        '''This just tests that the function itself works'''
         self.preamble()
         n_samples = 3
         sample = subsampler.convex_hull(self.data_matrix, self.num_pc, n_samples)
@@ -26,6 +26,25 @@ class subsampleTest(unittest.TestCase):
 
         sample = subsampler.convex_hull(self.data_matrix, self.num_pc, self.N_datapoints+1)
         self.assertLess(len(sample), self.N_datapoints)
+
+        manual_convex_hull_check = np.array(
+            [[0, 0, 0],
+             [1, 1, 1],
+             [2, 2, 2],
+             [1, 2, 1],
+             [2, 1, 2],
+             [3, 0, 0],
+             [0, 3, 0],
+             [0, 0, 3]]
+        )
+        sample = subsampler.convex_hull(manual_convex_hull_check, 2, len(manual_convex_hull_check))
+        pca = PCA(
+            n_components=2, svd_solver="arpack", random_state=0
+        ).fit_transform(manual_convex_hull_check)
+        vertices = np.unique([int(pca[:,0].argmin()), int(pca[:,0].argmax()), int(pca[:,1].argmin()), int(pca[:,1].argmax())])
+        vertices = torch.from_numpy(manual_convex_hull_check[vertices]).to(dtype=torch.float32)
+        self.assertTrue(all([i in sample for i in vertices]))
+
 
     def test_kmeans(self):
         '''This just tests that the function itself works'''
