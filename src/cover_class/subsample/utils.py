e4249f997f80cefc14e3f5f901ce5053794d083a
makiper <michael.kiper@jpl.nasa.gov>
2025-09-02 13:12:41 -0700

    made tts branch off of main repo develop branch

---
+++ src/cover_class/subsample/utils.py
@@ -0,0 +1,12 @@
+from typing import Tuple, Any
+from torch import FloatTensor, Tensor
+from numpy.typing import NDArray
+import numpy as np
+from sklearn.model_selection import train_test_split as tts # type: ignore[import]
+
+def interior_interpolation(data_matrix: NDArray[np.float32]) -> Tuple[FloatTensor, Tensor, FloatTensor, Tensor]: ... # type: ignore
+
+def train_test_split(data_matrix: FloatTensor, labels:Tensor, frac_test: float) -> Tuple[FloatTensor, FloatTensor, Tensor, Tensor]:
+    return tts(data_matrix, labels, test_size=frac_test)
+
+def prep_data_from_config(config:str) -> Tuple[FloatTensor, Tensor, FloatTensor, Tensor]: ... # type: ignore
