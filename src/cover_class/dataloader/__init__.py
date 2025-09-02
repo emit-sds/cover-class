9ae321ec2cba6b9b427ce256a568d4960786621c
makiper <michael.kiper@jpl.nasa.gov>
2025-09-02 11:02:48 -0700

    added dataloader classes to parent __init__

--- src/cover_class/dataloader/__init__.py
+++ src/cover_class/dataloader/__init__.py
@@ -1,7 +1,11 @@
 from cover_class.dataloader.dataloader import (
     OrchestratorDataLoader,
+    OrchestratorDataLoaderArgs,
+    dataloader_from_config
 )
 
 __all__ = [
     "OrchestratorDataLoader",
+    "OrchestratorDataLoaderArgs",
+    "dataloader_from_config",
 ]
